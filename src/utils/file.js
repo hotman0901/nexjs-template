905111869412154edc9993f66f4745b959ec949a
bennyhsieh <benny.hsieh@jyintgroup.com>
2021-11-05 13:42:05 +0800

    feat: update

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,11 +1,8 @@
-/* eslint-disable no-console */
-import { publicRuntimeConfig } from './runtimeConfig';
-
 /**
  * @description 靜態檔路徑，如果在 production 模式要改吃 cdn url
  * @returns string
  */
 export const getBaseCDN = () => {
-  const { CDN_URL } = publicRuntimeConfig;
-  return CDN_URL ? `${CDN_URL}/cdn/public` : '';
+  const url = process.env.CDN_URL || '';
+  return url ? `${url}/cdn/public` : '';
 };
