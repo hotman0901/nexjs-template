cd0ad0d039471350dd5ca29e95c6b129929785df
bennyhsieh <benny.hsieh@jyintgroup.com>
2021-11-15 15:13:29 +0800

    update feat: add framer motion

--- pages/motion/index.js
+++ pages/motion/index.js
@@ -1,67 +1,56 @@
-import { motion, useTransform, useMotionValue } from 'framer-motion';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
-const Example = function () {
-  const blockVariants = {
-    initial: {
-      rotate: 0,
-      x: 100,
-    },
-    target: {
-      rotate: 360,
-      x: 500,
-    },
-  };
+const WGContainer = styled.div`
+  background: linear-gradient(250deg, #7b2ff7, #f107a3);
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
 
-  const rotate = useMotionValue(0);
+const WGSwitch = styled.div`
+  width: 160px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.4);
+  display: flex;
+  justify-content: flex-start;
+  border-radius: 50px;
+  padding: 10px;
+  cursor: pointer;
+  justify-content: ${({ isOn }) => (isOn ? 'flex-end' : 'flex-start')};
 
-  const x = useMotionValue(0);
-  /**
-   * Here we tie together the value of "scale" to the value
-   * of "rotate"
-   * The scale will increase along the rotation, from 0
-   * until the rotation reaches 270 degrees ([0, 270])
-   * where the scale property will be equal to 1 ([0, 1]).
-   * The scale will stop increasing while the rotation
-   * finishes its transition
-   *
-   * You can try to modify the values below, and see how it
-   * impacts the resulting transition.
-   */
-  const scale = useTransform(rotate, [0, 360], [0, 1]);
+  .handle {
+    width: 80px;
+    height: 80px;
+    background-color: white;
+    border-radius: 40px;
+  }
+`;
 
-  const opacity = useTransform(x, [100, 500], [0, 1]);
-
-  setTimeout(() => {
-    x.set(150);
-  }, 5000);
-
-  return (
-    <motion.div
-      style={{
-        background: 'linear-gradient(90deg,#ffa0ae 0%,#aacaef 75%)',
-        height: '100px',
-        width: '100px',
-        borderRadius: '10px',
-        rotate,
-        scale,
-        opacity,
-        x,
-      }}
-      variants={blockVariants}
-      initial="initial"
-      animate="target"
-      transition={{
-        ease: 'easeInOut',
-        duration: 4,
-      }}
-    />
-  );
+const spring = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30,
 };
+
 const Motion = function () {
+  const [isOn, setIsOn] = useState(false);
+  const toggleSwitch = () => setIsOn(!isOn);
+
   return (
-    <div>
-      <Example />
-    </div>
+    <WGContainer>
+      <WGSwitch className="switch" isOn={isOn} onClick={toggleSwitch}>
+        {/* 要使用 layout 這個參數 轉換的 style 才會順暢 */}
+        <motion.div className="handle" layout transition={spring} />
+      </WGSwitch>
+    </WGContainer>
   );
 };
 
